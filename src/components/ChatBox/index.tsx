a9610e71978d4834792153261e79d74b2154451c
Eduardo <edu-henrique2014@hotmail.com>
2021-10-23 20:17:04 -0300

    ChatBox finished

--- src/components/ChatBox/index.tsx
+++ src/components/ChatBox/index.tsx
@@ -1,19 +1,21 @@
-import { FormEvent, useContext, useState } from 'react';
+import { FormEvent, useContext, useEffect, useState } from 'react';
 import { VscArrowLeft } from 'react-icons/vsc';
 import { FiSend } from 'react-icons/fi';
+import { api } from '../../services/api';
+import io from 'socket.io-client';
 
 import { AuthContext } from '../../contexts/auth';
-import { api } from '../../services/api';
+import { Message } from '../Message';
+import { Message as MessageProps, MessageContext } from '../../contexts/message';
 
 import styles from './styles.module.scss';
-import { Message } from '../Message';
-import { MessageContext } from '../../contexts/message';
 
 export function ChatBox() {
-  const [message, setMessage] = useState('');
-
   const { user } = useContext(AuthContext);
   const { openChat, closeChatBox } = useContext(MessageContext);
+
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState<MessageProps[]>(openChat ? openChat.messages : []);
 
   const contact = openChat?.users[0].id === user?.id ? openChat?.users[1] : openChat?.users[0];
 
@@ -25,12 +27,23 @@ export function ChatBox() {
     }
 
     await api.post('/chat/messages', {
-      chat_id: openChat?.id,
-      message
+      id: openChat?.id,
+      text: message
     });
 
     setMessage('');
   }
+
+  useEffect(() => {
+    const socket = io('http://localhost:4000');
+
+    socket.on(`new_private_message_${openChat?.id}`, (newMessage: MessageProps) => {
+      const updatedMessages = [...messages];
+      updatedMessages.push(newMessage);
+
+      setMessages(updatedMessages);
+    });
+  }, []);
 
   return (
     <div className={styles.sendMessageFormWrapper}>
@@ -49,7 +62,7 @@ export function ChatBox() {
 
       <div className={styles.messages}>
         {
-          openChat?.messages.map(message => (
+          messages.map(message => (
             <Message
               key={message.id}
               text={message.text}
